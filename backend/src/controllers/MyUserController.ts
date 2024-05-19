945135b00fb4434ad5081f57a11a76674fb9f883
Karaskaki <nik.ranchi7@gmail.com>
2024-05-19 21:55:21 +0530

    Update user profile API

--- backend/src/controllers/MyUserController.ts
+++ backend/src/controllers/MyUserController.ts
@@ -27,24 +27,31 @@ const createCurrentUser = async (req: Request, res: Response) => {
   }
 };
 
-// const logoutUser = async (req: Request, res: Response) => {
-//   try {
-//     const { auth0Id } = req.body;
-//     const existingUser = await User.findOne({ auth0Id });
+const updateCurrentUser = async (req: Request, res: Response) => {
+  try {
+    const { name, addressLine1, country, city } = req.body;
 
-//     if (existingUser) {
-//       const deleteId = await User.deleteOne({ existingUser });
-//       return res.status(200).send({
-//         success: deleteId,
-//       });
-//     }
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ message: "Error in Logout of user" });
-//   }
-// };
+    const user = await User.findById(req.userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    user.name = name;
+    user.addressLine1 = addressLine1;
+    user.country = country;
+    user.city = city;
+
+    await user.save();
+
+    res.send(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error in updateing user" });
+  }
+};
 
 export default {
   createCurrentUser,
-  // logoutUser,
+  updateCurrentUser,
 };
--- backend/src/routes/myUserRoute.ts
+++ backend/src/routes/myUserRoute.ts
@@ -1,8 +1,16 @@
 import Express from "express";
 import MyUserController from "../controllers/MyUserController";
-import { jwtCheck } from "../middleware/auth";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyUserRequest } from "../middleware/validation";
 
 const router = Express.Router();
 
 router.post("/", jwtCheck, MyUserController.createCurrentUser);
+router.put(
+  "/",
+  jwtCheck,
+  jwtParse,
+  validateMyUserRequest,
+  MyUserController.updateCurrentUser
+);
 export default router;
