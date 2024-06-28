612a839f58621d151f2c34b46e253a568a962fcf
Karaskaki <nik.ranchi7@gmail.com>
2024-06-28 23:39:41 +0530

    Pre-Populate User Form

--- frontend/src/api/MyUserApi.tsx
+++ frontend/src/api/MyUserApi.tsx
@@ -1,5 +1,5 @@
+import { User } from "@/types";
 import { useAuth0 } from "@auth0/auth0-react";
-import { error } from "console";
 import { useMutation, useQuery } from "react-query";
 import { toast } from "sonner";
 
@@ -8,7 +8,7 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export const useGetMyUser = () => {
   const { getAccessTokenSilently } = useAuth0();
 
-  const getMyUserRequest = async () => {
+  const getMyUserRequest = async (): Promise<User> => {
     const accessToken = await getAccessTokenSilently();
 
     const response = await fetch(`${API_BASE_URL}/api/my/user`, {
--- frontend/src/pages/UserProfilePage.tsx
+++ frontend/src/pages/UserProfilePage.tsx
@@ -3,10 +3,18 @@ import UserProfileForm from "@/forms/user-profile-form/UserProfileForm";
 
 function UserProfilePage() {
   const { currentUser, isLoading: isGetLoading } = useGetMyUser();
+  const { updateUser, isLoading: isUpdateLoading } = useUpdateMyUser();
 
   if (isGetLoading) return <span>Loading...</span>;
 
-  const { updateUser, isLoading: isUpdateLoading } = useUpdateMyUser();
-  return <UserProfileForm onSave={updateUser} isLoading={isUpdateLoading} />;
+  if (!currentUser) return <span>Unable to load User Profile</span>;
+
+  return (
+    <UserProfileForm
+      currentUser={currentUser}
+      onSave={updateUser}
+      isLoading={isUpdateLoading}
+    />
+  );
 }
 export default UserProfilePage;
