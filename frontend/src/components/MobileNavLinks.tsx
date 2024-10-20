80e319203310162a0ae68f910c2539fc0461f6f6
Nikhil Singh <95135482+karaskaki@users.noreply.github.com>
2024-10-20 21:31:50 +0530

    Update MobileNavLinks.tsx

--- frontend/src/components/MobileNavLinks.tsx
+++ frontend/src/components/MobileNavLinks.tsx
@@ -13,7 +13,9 @@ const MobileNavLinks = () => {
         User Profile
       </Link>
       <Button
-        onClick={() => logout()}
+        onClick={() => logout({                       
+          returnTo: window.location.origin;
+        })}
         className="flex item-center px-3 font-bold hover:bg-gray-500"
       >
         Log Out
